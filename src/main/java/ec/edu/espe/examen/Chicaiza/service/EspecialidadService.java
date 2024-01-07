86fec7a784a9f351712a07292866e194df7c2cad
katherineibeth <65550586+katherineibeth@users.noreply.github.com>
2024-01-06 21:12:54 -0500

    Update EspecialidadService.java

--- src/main/java/ec/edu/espe/examen/Chicaiza/service/EspecialidadService.java
+++ src/main/java/ec/edu/espe/examen/Chicaiza/service/EspecialidadService.java
@@ -1,25 +1,38 @@
 package ec.edu.espe.examen.Chicaiza.service;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ec.edu.espe.examen.Chicaiza.dao.EspecialidadRepository;
 import ec.edu.espe.examen.Chicaiza.domain.Especialidad;
+import ec.edu.espe.examen.Chicaiza.service.Exception.CreacionException;
+import jakarta.transaction.Transactional;
 
 @Service
 public class EspecialidadService {
 
-     @Autowired
-    private EspecialidadRepository especialidadRepository;
+    private final EspecialidadRepository especialidadRepository;
 
-    public void crearEspecialidad(Especialidad especialidad) {
-        especialidadRepository.save(especialidad);
+    public EspecialidadService(EspecialidadRepository especialidadRepository){
+        this.especialidadRepository = especialidadRepository;
     }
 
-    public List<Especialidad> obtenerEspecialidadesPorNombre(String patronNombre) {
-        return especialidadRepository.findByNombreContaining(patronNombre);
+    @Transactional
+    public Especialidad crearEspecialidad(Especialidad especialidad) {
+        try {
+            return this.especialidadRepository.save(especialidad);
+        } catch (Exception e) {
+            throw new CreacionException("No se pudo crear la Especialidad: " + especialidad.toString(), e);
+        }
+    }
+
+    public Especialidad buscarPatron(String patron) {
+        try{
+            return especialidadRepository.findByCodigo(patron);
+        } catch (Exception e) {
+            throw new RuntimeException("La Especialidad y Patron no existen: " + patron, e);
+        }
+        
     }
     
 }
