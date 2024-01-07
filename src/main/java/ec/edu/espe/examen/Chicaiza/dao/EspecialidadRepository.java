f217dac6cdbd93d4204154a00dff35a62913d347
katherineibeth <65550586+katherineibeth@users.noreply.github.com>
2024-01-06 20:59:46 -0500

    Update EspecialidadRepository.java

--- src/main/java/ec/edu/espe/examen/Chicaiza/dao/EspecialidadRepository.java
+++ src/main/java/ec/edu/espe/examen/Chicaiza/dao/EspecialidadRepository.java
@@ -1,12 +1,12 @@
 package ec.edu.espe.examen.Chicaiza.dao;
 
-import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import ec.edu.espe.examen.Chicaiza.domain.Especialidad;
 
-public interface EspecialidadRepository extends JpaRepository<Especialidad, String> {
 
-    List<Especialidad> findByNombreContaining(String patronNombre);
+public interface EspecialidadRepository extends CrudRepository<Especialidad, String> {
+
+    public Especialidad findByCodigo(String codEspecialidad);
 }
