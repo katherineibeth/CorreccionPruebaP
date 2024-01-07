0632995894e819c53d18b9461d4145d490c589ef
katherineibeth <65550586+katherineibeth@users.noreply.github.com>
2024-01-06 21:00:52 -0500

    Update MateriaRepository.java

--- src/main/java/ec/edu/espe/examen/Chicaiza/dao/MateriaRepository.java
+++ src/main/java/ec/edu/espe/examen/Chicaiza/dao/MateriaRepository.java
@@ -1,5 +1,9 @@
 package ec.edu.espe.examen.Chicaiza.dao;
 
-public class MateriaRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import ec.edu.espe.examen.Chicaiza.domain.Materia;
+
+public interface MateriaRepository extends CrudRepository<Materia, Integer> {
     
 }
