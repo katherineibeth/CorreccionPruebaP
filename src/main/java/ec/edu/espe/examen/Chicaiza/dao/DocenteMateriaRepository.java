1e4d203d0ae7f7cd29b1f8dadadd27bc1b71ba3b
katherineibeth <65550586+katherineibeth@users.noreply.github.com>
2024-01-06 21:00:11 -0500

    Update DocenteMateriaRepository.java

--- src/main/java/ec/edu/espe/examen/Chicaiza/dao/DocenteMateriaRepository.java
+++ src/main/java/ec/edu/espe/examen/Chicaiza/dao/DocenteMateriaRepository.java
@@ -1,5 +1,9 @@
 package ec.edu.espe.examen.Chicaiza.dao;
 
-public class DocenteMateriaRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import ec.edu.espe.examen.Chicaiza.domain.DocenteMateria;
+
+public interface DocenteMateriaRepository extends CrudRepository<DocenteMateria, Integer>{
     
 }
