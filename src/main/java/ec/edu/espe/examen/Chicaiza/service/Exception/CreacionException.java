535cc39e70b47b2e6ffe6ff35e5a75ec6a4a0d2e
katherineibeth <65550586+katherineibeth@users.noreply.github.com>
2024-01-06 21:11:01 -0500

    Add files via upload

---
+++ src/main/java/ec/edu/espe/examen/Chicaiza/service/Exception/CreacionException.java
@@ -0,0 +1,8 @@
+package ec.edu.espe.examen.Chicaiza.service.Exception;
+
+public class CreacionException  extends RuntimeException{
+
+    public CreacionException(String message, Exception cause){
+        super(message, cause);
+    }
+}
