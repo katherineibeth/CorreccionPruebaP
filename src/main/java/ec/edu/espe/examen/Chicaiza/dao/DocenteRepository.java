8b54160c62fb03f993442d991bab7c27631013f4
katherineibeth <65550586+katherineibeth@users.noreply.github.com>
2024-01-06 21:00:33 -0500

    Update DocenteRepository.java

--- src/main/java/ec/edu/espe/examen/Chicaiza/dao/DocenteRepository.java
+++ src/main/java/ec/edu/espe/examen/Chicaiza/dao/DocenteRepository.java
@@ -1,8 +1,9 @@
 package ec.edu.espe.examen.Chicaiza.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import ec.edu.espe.examen.Chicaiza.domain.Docente;
 
-public interface DocenteRepository extends JpaRepository<Docente, Integer> {
+public interface DocenteRepository extends CrudRepository<Docente, Integer> {
+    
 }
